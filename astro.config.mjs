9189a848bd1aefabdada0054db3d143198d6aa75
Eric Rodrigues Pires <eric@eric.dev.br>
2025-09-28 16:59:59 -0300

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from "astro/config";
-import tailwindIntegration from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://eric.dev.br",
-  integrations: [
-    tailwindIntegration({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [],
   build: {
     assets: "assets",
   },
   outDir: "./dist",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- tailwind.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: ["variant", "@media not print { .dark & }"],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [],
-};
