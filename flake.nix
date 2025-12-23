55ae5e62096a55bdc7e98829993ec96c3cf2d96f
Eric Rodrigues Pires <eric@eric.dev.br>
2025-12-23 19:11:56 -0300

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My personal static website.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        flakedPkgs = pkgs;
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_24
+          ];
+        };
+      }
+    );
+}
