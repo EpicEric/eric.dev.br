4fc049bfb4f53ef040d380eef04e739d729b7abd
Eric Rodrigues Pires <eric@eric.dev.br>
2025-12-25 21:11:20 -0300

    Working on new website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import alpinejs from "@astrojs/alpinejs";
+
 export default defineConfig({
   site: "https://eric.dev.br",
-  integrations: [],
+  integrations: [alpinejs()],
   build: {
     assets: "assets",
   },
